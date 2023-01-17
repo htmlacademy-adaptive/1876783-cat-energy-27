a8b1f622d50bc81f5404087d6abb6c42a7ed35e5
Anna Lopatina <anjuta377@gmail.com>
2023-01-17 14:30:28 +0300

    исправляет карты

--- source/js/script.js
+++ source/js/script.js
@@ -1,10 +1,11 @@
 let nav = document.querySelector('.navigation');
 let navToogle = document.querySelector('.navigation__toggle');
+let map = document.querySelector('.footer-address__wrapper-location');
 
 nav.classList.remove('navigation--nojs');
 
 navToogle.addEventListener('click', function () {
-  if (nav.classList.contains('navigation--closed')){
+  if (nav.classList.contains('navigation--closed')) {
     nav.classList.remove('navigation--closed');
     nav.classList.add('navigation--opened');
   } else {
@@ -12,3 +13,5 @@ navToogle.addEventListener('click', function () {
     nav.classList.remove('navigation--opened');
   }
 });
+
+map.classList.remove('footer-address__wrapper-location--nojs');
